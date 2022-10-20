dc9c6706ca5eafde152b4127bee4265cb5f8c0da
Francesco Giuseppe Ziccolella <52136996+Zicco99@users.noreply.github.com>
2022-10-20 10:45:16 +0200

    Update db_creation.sql

--- fetch_scripts/db_creation.sql
+++ fetch_scripts/db_creation.sql
@@ -1,7 +1,7 @@
 -- psql -U postgres f.ziccolella -f C:/path/to/db_create.sql
 
 -- Psql basic implementation is stupid, it uses operating system’s file system for its storage, so it would be better to localize the DB
-CREATE TABLESPACE deso_space OWNER f.ziccolella LOCATION '/home/local/ADUNIPI/datasets/deso'
+CREATE TABLESPACE deso_space OWNER <user> LOCATION '<path-of-database'
 
 -- Once created a new tablespace, we are allowed to create databases in it
 CREATE DATABASE deso_blockchain TABLESPACE deso_space;
